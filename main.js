5180230e01d9b6fe02fb4c692b0443e8a8b56182
JonMoore9000 <jonmoore9000@gmail.com>
2016-12-13 23:53:25 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+
+// Public Key = 373a048bdd4542dc2fb2432adcb916b8
+// Private Key = 813f40428b74bd23ff9927888703ca2af49248a5
+// Endpoint = https://gateway.marvel.com/v1/public/comics
+// Params = hash, key, timestamp
+
+// md5sum = d5c69bc4831d7712566df3eb08d35688
+
+
+var base_url = 'https://gateway.marvel.com/v1/public/characters'
+
+function getData(searchItem, callback) {
+	var query = {
+		url: base_url,
+		//'Cache-Control': 'max-age=0',
+		headers: {
+		'If-None-Match': '3045efddd7d36cf5be71d15c2e4de7954eda56ce',
+		},
+		data: {
+			hash: 'd5c69bc4831d7712566df3eb08d35688',
+			ts: '12122016',
+			apikey: '373a048bdd4542dc2fb2432adcb916b8',
+			nameStartsWith: searchItem,
+		},
+		type: 'GET',
+		dataType: 'json',
+		success: callback,
+	};
+	$.ajax(query);
+};
+
+
+function displayData(apiResults) {
+	console.log(apiResults);
+	var resultElement = '';
+	if(apiResults.data.results) {
+		resultElement = apiResults.data.results.map(function(item) {
+
+			var comics = item.comics.items.map(function(comic) {
+				return '<span class="comicTitles">' +comic.name + '<br></span>' ;
+			});
+
+			 return '<p>' + comics + '</p>';
+			});
+
+	}
+
+	else {
+		resultElement += '<p> No results </p>';
+	};
+
+	$('.js-query-results').html(resultElement);
+};
+
+
+function submitSearch() {
+	$('.js-form').submit(function(event) {
+		event.preventDefault();
+		var query = $(this).find('.js-query').val();
+		getData(query, displayData);
+	});
+
+};
+
+$(function (event) {
+	submitSearch();
+});
