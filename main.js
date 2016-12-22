6c83029c7e8389be7a081d6d6783efdcd95344a0
JonMoore9000 <jonmoore9000@gmail.com>
2016-12-22 15:57:20 -0600

    pagination added

--- main.js
+++ main.js
@@ -8,13 +8,18 @@
 
 // md5sum = d5c69bc4831d7712566df3eb08d35688
 
+// State
+
+var state = { currentOffset: 0 }
+
+
+// Api Function
 
 var base_url = 'https://gateway.marvel.com/v1/public/characters'
 
-function getData(searchItem, callback) {
+function getData(searchItem, callback, offset) {
 	var query = {
 		url: base_url,
-		//'Cache-Control': 'max-age=0',
 		headers: {
 		'If-None-Match': '3045efddd7d36cf5be71d15c2e4de7954eda56ce',
 		},
@@ -23,6 +28,8 @@ function getData(searchItem, callback) {
 			ts: '12122016',
 			apikey: '373a048bdd4542dc2fb2432adcb916b8',
 			nameStartsWith: searchItem,
+			limit: 5,
+			offset: offset,
 		},
 		type: 'GET',
 		dataType: 'json',
@@ -31,6 +38,8 @@ function getData(searchItem, callback) {
 	$.ajax(query);
 };
 
+
+// Dusplayng the Info From Api
 
 function displayData(apiResults) {
 	console.log(apiResults);
@@ -43,7 +52,7 @@ function displayData(apiResults) {
 			if (item.comics.items.length) {
 
 			 item.comics.items.forEach(function(comic) {
-				comics +=  + '<span class="comicTitles">' + comic.name + '<br></span>' ;
+				comics += '<span class="comicTitles">' + comic.name + '<br></span>' ;
 			});
 
 			}
@@ -51,11 +60,14 @@ function displayData(apiResults) {
 			else {
 				comics = '<p> No comics found </p>';
 			}
+			var description = item.description
+			description = description.replace(/<[^>]*>/, '');
 
-			 return '<p>' + '<strong class="heroName">' + item.name + '</strong>' + '<br>' 
-			 + '<p class="heroWords">' + item.description + '</p>' 
+
+			 return  '<strong class="heroName">' + item.name + '</strong>' + '<br>' 
+			 + '<p class="heroWords">' + description + '</p>' 
 			 + '<br>' + '<img class="heroImage" src="' 
-			 + item.thumbnail.path + '.jpg' + '">' + '<br>' + comics + '</p>';
+			 + item.thumbnail.path + '.jpg' + '">' + '<br>' + comics;
 
 			});
 
@@ -69,15 +81,52 @@ function displayData(apiResults) {
 };
 
 
+// Pagination Functions
+
+function nextPage(state) {
+		state.currentOffset += 5;
+		console.log(state.currentOffset);
+		var query = $('.js-query').val();
+		getData(query, displayData, state.currentOffset);
+	}
+
+function prevPage(state) {
+		if (state.currentOffset >= 5) {
+		state.currentOffset -= 5;
+		var query = $('.js-query').val();
+		getData(query, displayData, state.currentOffset);
+	}
+}
+
+// Event Handlers
+
+function forwardPage() {
+	$('.next').click(function(event) {
+		event.preventDefault();
+		nextPage(state);
+	});
+}
+
+function backPage() {
+	$('.prev').click(function(event) {
+		event.preventDefault();
+		prevPage(state);
+	});
+}
+
+
 function submitSearch() {
 	$('.js-form').submit(function(event) {
 		event.preventDefault();
-		var query = $(this).find('.js-query').val();
-		getData(query, displayData);
+		var query = $('.js-query').val();
+		getData(query, displayData, 0);
 	});
 
 };
 
+
 $(function (event) {
 	submitSearch();
+	forwardPage();
+	backPage();
 });
